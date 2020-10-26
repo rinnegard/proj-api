8a78497a9a3b8922d6d09e72c9094fd76e4101da
Victor <victor.rinnegard@hotmail.com>
2020-10-26 20:20:12 +0100

    Added basic structure in app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+
+const app = express();
+const port = 1338;
+
+const corsOptions = {
+  exposedHeaders: 'Authorization',
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use((req, res, next) => {
+    console.log(req.method);
+    console.log(req.path);
+    next();
+});
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    if (res.headersSent) {
+        return next(err);
+    }
+
+    res.status(err.status || 500).json({
+        "errors": [
+            {
+                "status": err.status,
+                "title":  err.message,
+                "detail": err.message
+            }
+        ]
+    });
+});
+
+// Start up server
+const server = app.listen(port, () => console.log(`Example API listening on port ${port}!`));
